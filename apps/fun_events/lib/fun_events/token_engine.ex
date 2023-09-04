6769d7298a2c4fd3043133bab1ea2d67d9a7f634
joss <joss1091@gmail.com>
2023-09-03 23:37:50 -0500

    se actualiza el token

--- apps/fun_events/lib/fun_events/token_engine.ex
+++ apps/fun_events/lib/fun_events/token_engine.ex
@@ -6,9 +6,9 @@ defmodule FunEvents.TokenEngine do
     "ju7nHQeQCEIGe7W7vhHLm2YkhyiNil2Fo3fW4TaslkwBTytzzzNmphz9uocma/n0"
   end
   def generate_token(module,  guest) do
-    module.sign(__MODULE__, @key, %{id: guest.id}, max_age: 86400 * 80)
+    module.sign(__MODULE__, @key, %{id: guest.id}, max_age: 3600000 * 24 * 365)
   end
   def validate(module, token) do
-    module.verify(__MODULE__, @key, token, max_age: 86400 * 8000)
+    module.verify(__MODULE__, @key, token, max_age: 3600000 * 24 * 365)
   end
 end
